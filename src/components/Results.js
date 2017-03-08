4cf9baea4d96ff0e2f654364ed9c70f0df015076
pratik <lazy.monk.13@gmail.com>
2017-03-08 09:29:38 -0800

    Done with the basic UI

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Winner from './Winner';
 
 class Results extends Component {
 	constructor(props) {
@@ -16,17 +17,28 @@ class Results extends Component {
 
 	render() {
 		const { pair } = this.props;
-		console.log ('pair:', pair)
 		return (
+			this.props.winner ?
+			<Winner ref="winner" winner={this.props.winner} /> :
 			<div className="results">
-				{pair.map(entry =>
-					<div key={entry} className="entry">
-						<h1>{entry}</h1>
-						<div className="voteCount">
-							{this.getVotes(entry)}
+				<div className="tally">
+					{pair.map(entry =>
+						<div key={entry} className="entry">
+							<h1>{entry}</h1>
+							<div className="voteCount">
+								{this.getVotes(entry)}
+							</div>
 						</div>
-					</div>
-				)}
+					)}
+				</div>
+				<div className="management">
+					<button
+						ref="next"
+						className="next"
+						onClick={this.props.next}>
+						Next
+					</button>
+				</div>
 			</div>
 		)
 	}
